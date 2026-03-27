e5231642dbc7652c97a83c41d5731373ec0d652c
nikol4s <nikolassc4mpos@gmail.com>
2026-03-27 14:20:51 -0300

    feat: add toggle action for task completion

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,4 +1,6 @@
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Task
 from .permissions import IsOwner
@@ -9,6 +11,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
+        print(self.request.user)
         return Task.objects.filter(owner=self.request.user)
 
     def get_serializer_class(self):
@@ -18,3 +21,10 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=["patch"], url_path="toggle")
+    def toggle_done(self, request, pk=None):
+        task = self.get_object()
+        task.is_done = not task.is_done
+        task.save(update_fields=["is_done"])
+        return Response({"is_done": task.is_done})
