ca488fa315387e03d61afb658ee030502055a19a
nikol4s <nikolassc4mpos@gmail.com>
2026-03-27 14:05:38 -0300

    feat: implement task core view logic

--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Category
+from .permissions import IsOwner
+from .serializers import CategoryReadSerializer, CategoryWriteSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CategoryReadSerializer
+        return CategoryWriteSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,5 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/",   include("users.urls")),
-    path("api/", include("tasks.urls"))
+    path("api/", include("tasks.urls")),
+    path("api/", include("categories.urls")),
+
 ]
